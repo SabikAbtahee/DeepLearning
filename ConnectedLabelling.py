7a5a7e166fc10055fcc6cb76286d01261bfe5f74
SabikAbtahee <bsse0829@iit.du.ac.bd>
2018-11-12 07:35:38 +0600

    Segmentv2

--- ConnectedLabelling.py
+++ ConnectedLabelling.py
@@ -1,36 +1,65 @@
 import cv2
+import sys
 import numpy as np
 import copy
 from PIL import Image
 import matplotlib.pyplot as plt
+# from PredictFromModel import predict
+import operator
 
-def showImages(usefullImages):
+
+def savePositionOfImages(leftToRightPosition,center,new_image,positions):
+    # x=int((xhigh+xlow)/2)
+    # y=int((yhigh+ylow)/2)
+    x=leftToRightPosition
+    y=center
+    centerImage=[y,new_image]
+    positions[x]=centerImage
+    return positions
+
+def showImages(usefullImages,sortedX):
     # print(len(usefullImages))
-    for i in range(len(usefullImages)):
-        plt.imshow(usefullImages[i])
+    for i in sortedX:
+        # print(i[0])
+        plt.imshow(usefullImages[i[0]])
         plt.show()
     # plt.imshow(usefullImages[i])
     # plt.show()
 def rotate(usefullImages):
-    pix = np.array(usefullImages[0])
-    rotated = np.rot90(pix, 1)
-    print(rotated)
-    new_image = Image.fromarray(rotated)
-    new_image = new_image.convert("L")
 
-def deleteUnnecessaryImages(zoomedInImages):
+    pix = np.array(usefullImages)
+
+    rotated=[]
+    for i in range(4):
+        rotated.append(np.rot90(pix, i+1))
+        new_image = Image.fromarray(rotated[i])
+        plt.imshow(new_image)
+        plt.show()
+
+
+
+
+def deleteUnnecessaryImages(zoomedInImages,positions):
     usefullImages=[]
+    usefullPositions={}
+    x=0
     for i in range(len(zoomedInImages)):
         width, height = zoomedInImages[i].size
         if(width>0 and height>0):
             usefullImages.append(zoomedInImages[i])
-    return usefullImages
+            usefullPositions[x]=positions[i]
+            x+=1
+    return usefullImages,usefullPositions
 
 
 
 def makeZoomInImages(allImages):
     zoomedInImages=[]
+
     for i in range(len(allImages)):
+        # h,w = allImages[i].size
+        #
+        # print(h,w)
         pix = np.array(allImages[i])
         u=np.argwhere(pix>250)
         a=np.min(u,axis=0)
@@ -39,6 +68,7 @@ def makeZoomInImages(allImages):
         xhigh=b[0]
         ylow=a[1]
         yhigh=b[1]
+
         y=xlow-100
         w=(xhigh-xlow)+200
         x=ylow-100
@@ -54,28 +84,81 @@ def connectedComponents(image):
 
     image = image.astype( 'uint8' )
     nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(image, connectivity=4)
-    sizes = stats[1:, -1]; nb_components = nb_components - 1
-    min_size = 180
+
+    sizes = stats[1:, -1 ]; nb_components = nb_components - 1
+    # print(type(sizes[0]))
+    # print(sizes[0])
+    # print(type(output))
+    # print(output)
+    # print(type(nb_components))
+    print(nb_components)
+    min_size = 150
     MainImage3 = np.zeros(( output.shape ))
     allImages=[]
+    allnewImages={}
+    positions={}
     for i in range(0, nb_components):
         if sizes[i] >= min_size:
             MainImage3[output == i + 1] = 255
+            center=np.where(MainImage3==255)[0][0]
+            leftToRightPosition=np.where(MainImage3 == 255)[1][0]
+            # print(leftToRightPosition,center)
+
+
             new_image = Image.fromarray(MainImage3)
             new_image = new_image.convert("L")
+            positions = savePositionOfImages(leftToRightPosition, center, new_image, positions)
+            # plt.imshow(new_image)
+            # plt.show()
             MainImage3[output == i + 1] = 0
+
+            # allnewImages[leftToRightPosition]=new_image
             allImages.append(new_image)
+    maxw=0
+    check=True
+    for key, value in sorted(positions.items()):
+        print(key)
+        if(check==True):
+            maxw=value[0]
+            check=False
 
 
-    return allImages
+    return allImages,positions
 
 def readImage(testImageFileName):
     MainImage = cv2.imread(testImageFileName,cv2.IMREAD_GRAYSCALE)
     MainImage2=copy.deepcopy(MainImage)
-    MainImage2[MainImage < 120] = 255
-    MainImage2[MainImage>=120]=0
-
+    MainImage2[MainImage < 80] = 255
+    MainImage2[MainImage>=80]=0
+    # print(np.where(MainImage2>120))
+    # print(MainImage2[1395][1365])
     return MainImage2
+
+
+
+
+def predictionsIndividual(usefullImages):
+    for i in range(len(usefullImages)):
+        rotatedImages=rotate(usefullImages[i])
+
+    # showImages(usefullImages)
+    # predict()
+
+def  sortPositions(usefullPositions):
+    xposition={}
+    yposition={}
+    listValue=[]
+    for key, value in usefullPositions.items():
+        xposition[key]=value[0]
+        yposition[key]=value[1]
+
+    sorted_x = sorted(xposition.items(), key=operator.itemgetter(1))
+
+    # for key, value in sorted_x.items():
+    #     listValue.append(key)
+    #
+    # print(listValue)
+    return sorted_x
 
 
 def main():
@@ -84,11 +167,17 @@ def main():
     testImageFileName = testImagePath + 'Equ1.jpg'
 
     MainImage2=readImage(testImageFileName)
-    allImages=connectedComponents(MainImage2)
+    allImages,positions=connectedComponents(MainImage2)
+
     zoomedInImages = makeZoomInImages(allImages)
-    usefullImages=deleteUnnecessaryImages(zoomedInImages)
-    rotatedImages=rotate(usefullImages)
-    # showImages(usefullImages)
+
+
+
+    usefullImages,usefullPositions=deleteUnnecessaryImages(zoomedInImages,positions)
+    sortedX=sortPositions(usefullPositions)
+
+    showImages(usefullImages,sortedX)
+    # predictionsIndividual(usefullImages)
 
 if __name__ == "__main__":
     main()
--- PredictFromModel.py
+++ PredictFromModel.py
@@ -1,27 +1,30 @@
-from keras.models import load_model
-
-modelPath='C:\\Users\\sabik\\PycharmProjects\\DeepLearningBasics\\MainModel1.h5'
-imagepath='C:\\Users\\sabik\\PycharmProjects\\DeepLearningBasics\\TrainTestImages\\Train\\'
-filename= imagepath + '4\\4 (5).jpg'
-
-classifier = load_model(modelPath)
-classifier.get_config()
+# from keras.models import load_model
+#
+# modelPath='C:\\Users\\sabik\\PycharmProjects\\DeepLearningBasics\\MainModel1.h5'
+# imagepath='C:\\Users\\sabik\\PycharmProjects\\DeepLearningBasics\\TrainTestImages\\Train\\'
+# filename= imagepath + '4\\4 (5).jpg'
+#
+# classifier = load_model(modelPath)
+# classifier.get_config()
 from keras.preprocessing import image
 import numpy as np
 
-count=0
+# count=0
+#
+# for i in range(1,5917,1):
+#     filename = imagepath + '4\\4 ('+str(i)+').jpg'
+#
+#     test_image = image.load_img(filename, target_size = (45, 45))
+#     test_image = image.img_to_array(test_image)
+#     test_image = np.expand_dims(test_image, axis = 0)
+#     type(test_image)
+#     result = classifier.predict(test_image)
+#     result
+#     if(result[0][10]>=0.9):
+#         count+=1
+#
+# print(count)
 
-for i in range(1,5917,1):
-    filename = imagepath + '4\\4 ('+str(i)+').jpg'
-
-    test_image = image.load_img(filename, target_size = (45, 45))
-    test_image = image.img_to_array(test_image)
-    test_image = np.expand_dims(test_image, axis = 0)
-    type(test_image)
-    result = classifier.predict(test_image)
-    result
-    if(result[0][10]>=0.9):
-        count+=1
-
-print(count)
-
+def predict(image):
+    # result = classifier.predict(image)
+    print("Heello")
